fd00555edaa3aca13bdaf6819b6301b422138913
Aniket Das <dasaniket971@gmail.com>
2025-06-23 14:17:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -85,3 +85,22 @@ if (registerForm) {
         }
     });
 }
+
+document.oncontextmenu = () => {
+    return false
+}
+
+document.onkeydown = e => {
+    if(e.key == "F12"){
+        return false
+    }
+    if(e.ctrlKey && e.key == "u"){
+        return false
+    }
+    if(e.ctrlKey && e.key == "c"){
+        return false
+    }
+    if(e.ctrlKey && e.Key == "v"){
+        return false
+    }
+}
